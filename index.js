7bb43703ee1c2921856290141a17f6559d57dd27
HSenarath <hirunisenarath@gmail.com>
2022-10-13 22:06:33 -0400

    Ensure that copy button can't be used unless there is a password generated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // all possible character values that can be used to generate a random password
-const characters =[
+const allCharacters =[
     'A','B','C','D','E','F','H','I','J','K','L','M','N','O','P','Q','R','S','T','U',
     'V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','n',
     'o','p','q','r','s','t','u','v','w','x','y','z','*','&','$','#','!','?','<','>','+'
     ];
 
 const passwordLengthEl = document.getElementById("password-length");
+
+// variables for copy button
 const copyMessageEl = document.getElementById("copied-message");
+let isPasswordCreated = false;
 
 // variables storing button elements
 const generatorBtn = document.getElementById("generator-btn");
@@ -18,19 +21,23 @@ let passwordGenerated = document.getElementById("password-generated");
 generatorBtn.addEventListener("click", function (){
     let passwordElements = "";
     let passwordLength = passwordLengthEl.value;
+    
 
     for (i = 0; i < passwordLength; i++) {
         // outputs random number between 1 and length of the character array
-        let randomNumber = Math.floor(Math.random() * characters.length) + 1
-        passwordElements += characters[randomNumber]
+        let randomNumber = Math.floor(Math.random() * allCharacters.length) + 1
+        passwordElements += allCharacters[randomNumber]
     }
     passwordGenerated.value = passwordElements
+    isPasswordCreated = true;
 })
 
 copyBtn.addEventListener("click", function (){
-    let input = passwordGenerated.value;
+    if (isPasswordCreated === true) {
+        let input = passwordGenerated.value;
 
-    navigator.clipboard.writeText(input);
-
-    copyMessageEl.textContent = "Copied Successfully";
+        navigator.clipboard.writeText(input);
+    
+        copyMessageEl.textContent = "Copied Successfully";
+    }
 })
